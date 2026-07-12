c638eb369b31f427e7a970bbe37c3eb86f59d3f8
Avinash <mishraavinash394@gmail.com>
2026-07-12 23:04:07 +0530

    Enhanced SOS page with Google Maps and emergency contacts

--- js/sos.js
+++ js/sos.js
@@ -1,15 +1,17 @@
 let sosButton = document.getElementById("sosButton");
-
+let cancelBtn = document.getElementById("cancelBtn");
 let locationDiv = document.getElementById("location");
 
 let countdown;
+let seconds = 5;
 
+// SOS Button Click
 sosButton.addEventListener("click", function () {
 
-    let seconds = 5;
+    seconds = 5;
 
     sosButton.disabled = true;
-
+    cancelBtn.style.display = "inline-block";
     sosButton.innerHTML = "SOS in " + seconds;
 
     countdown = setInterval(function () {
@@ -26,37 +28,75 @@ sosButton.addEventListener("click", function () {
 
         }
 
-    },1000);
+    }, 1000);
 
 });
 
-function sendSOS(){
+// Cancel SOS
+cancelBtn.addEventListener("click", function () {
 
-    if(navigator.geolocation){
+    clearInterval(countdown);
 
-        navigator.geolocation.getCurrentPosition(function(position){
+    sosButton.disabled = false;
+    sosButton.innerHTML = "ACTIVATE SOS";
+
+    cancelBtn.style.display = "none";
+
+    locationDiv.innerHTML =
+    "<p style='color:green;'>✅ SOS Cancelled Successfully.</p>";
+
+});
+
+// Send SOS
+function sendSOS() {
+
+    cancelBtn.style.display = "none";
+
+    if (navigator.geolocation) {
+
+        navigator.geolocation.getCurrentPosition(function (position) {
 
             let lat = position.coords.latitude;
-
             let lon = position.coords.longitude;
+let contacts =
+JSON.parse(localStorage.getItem("contacts")) || [];
+
+let contactHTML = "";
+
+contacts.forEach(contact => {
+
+    contactHTML += `
+    <p>👤 ${contact.name} - ${contact.phone}</p>
+    `;
+
+});
+            sosButton.disabled = false;
+            sosButton.innerHTML = "ACTIVATE SOS";
 
             locationDiv.innerHTML = `
+                <h3>🚨 SOS Activated</h3>
 
-            <h3>🚨 SOS Activated</h3>
+                <p><b>Latitude:</b> ${lat}</p>
 
-            <p><b>Latitude:</b> ${lat}</p>
+                <p><b>Longitude:</b> ${lon}</p>
+<p>
+<a href="https://www.google.com/maps?q=${lat},${lon}"
+target="_blank">
+📍 Open Live Location in Google Maps
+</a>
+</p>
 
-            <p><b>Longitude:</b> ${lon}</p>
+                <p style="color:red;font-weight:bold;">
+                Emergency Alert Sent Successfully!
+                </p>
+                <h3>📞 Emergency Contacts Alerted</h3>
 
-            <p style="color:red;font-weight:bold;">
-            Emergency Alert Sent Successfully!
-            </p>
-
+${contactHTML}
             `;
 
         });
 
-    }else{
+    } else {
 
         alert("Location not supported.");
 
