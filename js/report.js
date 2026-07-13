b0ba4c7010b4f1f170201f24fb4954784949a088
Avinash <mishraavinash394@gmail.com>
2026-07-13 13:11:31 +0530

    Short description of changes

--- js/report.js
+++ js/report.js
@@ -1,3 +1,17 @@
+if (navigator.geolocation) {
+
+    navigator.geolocation.getCurrentPosition(function(position){
+
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
+        document.getElementById("locationInput").value =
+        lat + ", " + lon;
+
+    });
+
+}
+
 const form = document.getElementById("reportForm");
 
 form.addEventListener("submit", function(e){
