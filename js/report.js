3ae908b8312095b40d9040763a9c3b9debcbef7a
Avinash <mishraavinash394@gmail.com>
2026-07-13 12:23:19 +0530

    Short description of changes

--- js/report.js
+++ js/report.js
@@ -3,9 +3,15 @@ const form = document.getElementById("reportForm");
 form.addEventListener("submit", function(e){
 
 e.preventDefault();
+let currentDate = new Date();
 
-document.getElementById("successMessage").innerHTML =
-"✅ Your report has been submitted successfully.";
+let dateTime = currentDate.toLocaleString();
+
+document.getElementById("successMessage").innerHTML = `
+✅ Your report has been submitted successfully.
+<br><br>
+📅 Report Time: ${dateTime}
+`;
 
 form.reset();
 
